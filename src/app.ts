65b1127b7f43179f27169eae5fe6c97eaac134bb
imtanvir <tanvirparvej101@gmail.com>
2024-07-09 08:33:47 +0600

    Project Initial and ready to go

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express, { Application } from "express";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
+
+const app: Application = express();
+const port = 3000;
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Sport Goods Backend!");
+});
+
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`Sport Goods Backend running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+process.on("unhandledRejection", (err) => {
+  console.log(`UnahandledRejection is detected , shutdown ...`, err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log(`UncaughtException is detected , shutdown ...`);
+  process.exit(1);
+});
